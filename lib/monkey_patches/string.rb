d52625a504edffa1ab07552d725a673b9e3a70de
Patrick Robinson <patrick.robinson@envato.com>
2017-01-03 13:58:12 +1100

    Replicate Active Support Monkey Patches of String.
    Provide a very basic mechanism to pluralise/singularise strings

---
+++ lib/monkey_patches/string.rb
@@ -0,0 +1,46 @@
+class String
+  def camelize
+    string = self
+    string = string.sub(/^[a-z\d]*/) { |match| match.capitalize }
+    string.gsub!(/(?:_|(\/))([a-z\d]*)/i) { "#{$1}#{$2.capitalize}" }
+    string.gsub!("/".freeze, "::".freeze)
+    string
+  end
+
+  def underscore
+    camel_cased_word = self
+    return camel_cased_word unless /[A-Z-]|::/.match?(camel_cased_word)
+    word = camel_cased_word.to_s.gsub("::".freeze, "/".freeze)
+    word.gsub!(/(?:(?<=([A-Za-z\d]))|\b)(?=\b|[^a-z])/) { "#{$1 && '_'.freeze }#{$2.downcase}" }
+    word.gsub!(/([A-Z\d]+)([A-Z][a-z])/, '\1_\2'.freeze)
+    word.gsub!(/([a-z\d])([A-Z])/, '\1_\2'.freeze)
+    word.tr!("-".freeze, "_".freeze)
+    word.downcase!
+    word
+  end
+
+  def demodulize
+    path = self
+    if i = path.rindex("::")
+      path[(i + 2)..-1]
+    else
+      path
+    end
+  end
+
+  def pluralize
+    if self[-1] == "s"
+      self
+    else
+      "#{self}s"
+    end
+  end
+
+  def singularize
+    if self[-1] == "s"
+      self.gsub(/s$/, "")
+    else
+      self
+    end
+  end
+end
