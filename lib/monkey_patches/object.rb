ecdf843448546ac9eb5d80a33641d9cffdb1a727
Patrick Robinson <patrick.robinson@envato.com>
2017-01-03 13:59:19 +1100

    Copy ActiveSupports deep_dup mechanism
    Just try to duplicate an object and if we fail, return the original object.
    We don't need to care about the performance optimisations provided by ActiveSupport

---
+++ lib/monkey_patches/object.rb
@@ -0,0 +1,14 @@
+class Object
+  # Returns a deep copy of object if it's duplicable. If it's
+  # not duplicable, returns +self+.
+  #
+  #   object = Object.new
+  #   dup    = object.deep_dup
+  #   dup.instance_variable_set(:@a, 1)
+  #
+  #   object.instance_variable_defined?(:@a) # => false
+  #   dup.instance_variable_defined?(:@a)    # => true
+  def deep_dup
+    self.dup rescue self
+  end
+end
